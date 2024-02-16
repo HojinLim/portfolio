fc93542b1d88f08ecc95a57d450849fa89f7c727
HojinLim <hojinim@gmail.com>
2024-02-16 13:01:16 +0900

    Design/인트로 제목 anim

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,23 +1,24 @@
-import React, { Suspense } from 'react';
 import AuroraEffect from './AuroraEffect';
-import Spline from '@splinetool/react-spline';
 
 type Props = {};
 
 const Intro = (props: Props) => {
-  // const onHandle= (e: SplineEvent)
   return (
     <div className="w-full h-[600px] bg-black flex justify-center items-center">
-      {/* <Spline scene="https://prod.spline.design/A6JGsW2hSlFA410A/scene.splinecode"> */}
       <h4 className="text-3xl sm:text-4xl font-bold relative overflow-hidden text-white border-none text-center">
-        안녕하세요. <br />
-        <br />
-        꼼꼼한 성격의 프론트엔드 개발자입니다.
-        <br />
-        <br /> 기록을 자주하는 습관이 저의 장점입니다.
+        <span className="animated-text" style={{ animationDelay: '0.5s' }}>
+          안녕하세요.
+        </span>
+        <br /> <br />
+        <span className="animated-text" style={{ animationDelay: '1s' }}>
+          꼼꼼한 성격의 프론트엔드 개발자입니다.
+        </span>
+        <br /> <br />
+        <span className="animated-text" style={{ animationDelay: '1.5s' }}>
+          기록을 자주하는 습관이 저의 장점입니다.
+        </span>
         <AuroraEffect />
       </h4>
-      {/* </Spline> */}
     </div>
   );
 };
