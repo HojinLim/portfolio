d1989fb8888d7b3171ac2cf6ae2592bc9eff6e27
HojinLim <hojinim@gmail.com>
2024-02-17 14:20:18 +0900

    Fix/스킬 컨테이너 비율

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,26 +11,27 @@ type Props = {};
 
 const Skills = (props: Props) => {
   const skillContainers = [
-    { value: 'Frontend', imgUrl: front, height: 'h-[650px]' },
-    { value: 'Backend', imgUrl: back, height: 'h-[650px]' },
-    { value: 'Mobile App', imgUrl: mobile, height: 'h-[250px]' },
-    { value: 'Deployment', imgUrl: deployment, height: 'h-[250px]' },
-    { value: 'Version Control', imgUrl: version, height: 'h-[250px]' }
+    { value: 'Frontend', imgUrl: front },
+    { value: 'Backend', imgUrl: back },
+    { value: 'Mobile App', imgUrl: mobile },
+    { value: 'Deployment', imgUrl: deployment },
+    { value: 'Version Control', imgUrl: version }
   ];
 
   return (
     <article id="skills" className="w-full py-10 bg-pink-300">
       <ArticleHeader name="skills" />
 
-      <section className="flex flex-wrap justify-center items-start">
+      <section id="skills-container" className="flex flex-wrap justify-center">
         {skillContainers.map((item, index) => (
           <div
             key={index}
-            className={`p-8 m-4 bg-white rounded-xl shadow-lg hover:shadow-lg transition duration-300 ease-in-out transform hover:translate-y-1 ${item.height} ${index < 2 ? 'w-72' : 'w-48'}`}
+            className={`p-8 m-4 bg-white rounded-xl shadow-lg hover:shadow-lg transition duration-300 ease-in-out transform hover:translate-y-1 mx-auto`}
+            style={{ width: '15rem' }}
           >
             <div className="mx-auto font-bold text-yellow-600 text-2xl mb-4">{item.value}</div>
-            <div className="w-full h-1 bg-black my-5"></div>
-            <img src={item.imgUrl} alt={item.value} className="object-contain mb-4 w-full" />
+            <div className="w-full h-1 bg-black"></div>
+            <img src={item.imgUrl} alt={item.value} className="object-contain w-full" />
           </div>
         ))}
       </section>
